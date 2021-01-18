57db22084f90065cfb480dc06e86737d840b5775
lindaf3 <linda.fan.4556@gmail.com>
2021-01-18 12:09:21 -0800

    Added New Activity

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,21 +3,28 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Parcel
 public class Movie {
     String posterPath;
     String title;
     String overview;
     String backdropPath;
+    double rating;
+    //empty constructor due to parcel
+    public Movie(){
+
+    }
 
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
 
     }
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -42,5 +49,8 @@ public class Movie {
     }
     public String getBackdropPath(){
         return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+    }
+    public double getRating(){
+        return rating;
     }
 }
